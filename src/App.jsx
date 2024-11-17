f1fd4df1ac0162716764c78bfb3f801c1fe370a1
akashpratape <akashpratape01@gmail.com>
2024-11-17 21:43:14 +0530

    Initial commit setup for webpage done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./background.css"
+
+function App() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,6 @@
+import React, { useState} from  "react";
+import "./Navbar.css"
+import Lottie from "lottie-react";
+import goToCart from "../../assets/icons/goToCart.json"
+
+
