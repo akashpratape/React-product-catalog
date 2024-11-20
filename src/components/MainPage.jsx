41ef43c8e7a78ef5b692ab56ee750d9e7489688a
akashpratape <akashpratape01@gmail.com>
2024-11-20 13:02:51 +0530

    complete project by removing all unnecessary code

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,23 +1,3 @@
-// import React from "react";
-// import "../background.css";
-// import Navbar from "./Navbar/Navbar.jsx";
-// import Filterbar from "./Filterbar/Filterbar.jsx";
-// import ProductCard from "./Productcard/Productcard.jsx";
-
-// function MainPage() {
-//     return (
-//         <div>
-//             <Navbar />
-//             <Filterbar />
-//             <ProductCard />
-//         </div>
-
-//     )
-// }
-
-// export default MainPage;
-
-
 import React, { useState } from "react";
 import "../background.css";
 import Navbar from "./Navbar/Navbar.jsx";
